1df0e50fef5f58166ce9118eae5efcf96bca24d2
Dominick <dguzzo@gmail.com>
2012-12-07 12:17:30 -0800

    added image compositing sample
    currently requires two-hardcoded image files. will make this more
    programmatic later.

--- rmagick-test-compositing.rb
+++ rmagick-test-compositing.rb
@@ -1,20 +1,45 @@
 require 'RMagick'
 
-gold_fill = Magick::GradientFill.new(0, 0, 0, 0, "#f6e09a", "#cd9245")
-red_fill = Magick::GradientFill.new(0, 0, 0, 0, "#ff0000", "#ff8888")
+def gradient_compositing_sample
+  gold_fill = Magick::GradientFill.new(0, 0, 0, 0, "#f6e09a", "#cd9245")
+  red_fill = Magick::GradientFill.new(0, 0, 0, 0, "#ff0000", "#ff8888")
 
-dst = Magick::Image.new(128, 128, gold_fill)
-src = Magick::Image.new(128, 128, red_fill)
-#src = Magick::Image.read("composite1-src.gif")[0]
+  dst = Magick::Image.new(128, 128, gold_fill)
+  src = Magick::Image.new(128, 128, red_fill)
+  #src = Magick::Image.read("composite1-src.gif")[0]
 
-# result = dst.composite(src, Magick::CenterGravity, Magick::OverCompositeOp)
-# result = dst.composite(src, 0, 0, Magick::OverCompositeOp)
-# result.write('./images/composites/composite1.gif')
+  # result = dst.composite(src, Magick::CenterGravity, Magick::OverCompositeOp)
+  # result = dst.composite(src, 0, 0, Magick::OverCompositeOp)
+  # result.write('./images/composites/composite1.gif')
 
-print "\nbeginning composites processing"
-Magick::CompositeOperator.values.each_with_index do |composite_style, index|
-  print '.'
-  result = dst.composite(src, 0, 0, composite_style)
-  result.write("./images/composites/composite#{index}.gif")
+  print "\nbeginning composites processing"
+  Magick::CompositeOperator.values.each_with_index do |composite_style, index|
+    print '.'
+    result = dst.composite(src, 0, 0, composite_style)
+    result.write("./images/gradient-composites/composite#{index}.gif")
+  end
+  puts "\ndone!"
 end
-puts "\ndone!"
+
+def image_compositing_sample(num_operations = 5, append_op_to_filename = false)
+  
+  dst_name = "Hard Pill"
+  src_name = "Turquoise Skies"
+  
+  dst = Magick::Image.read("./images/#{dst_name}.jpg").first
+  src = Magick::Image.read("./images/#{src_name}.jpg").first
+  
+  puts "beginning composites processing, using #{num_operations}"
+  # first two CompositeOperator are basically no-ops, so skip 'em
+  Magick::CompositeOperator.values[2..(num_operations+2)].each_with_index do |composite_style, index|
+    print '.'
+    append_string = append_op_to_filename ? composite_style.to_s : index
+    result = dst.composite(src, 0, 0, composite_style)
+    result.write("./images/image-composites/#{dst_name}-#{src_name}-#{append_string}.jpg")
+  end
+  puts "\ndone!"
+  
+end
+
+# gradient_compositing_sample
+image_compositing_sample(10)
