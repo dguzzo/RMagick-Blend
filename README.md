54da6e1356decc9e66f94fb869aea097592258d2
Dominick <dguzzo@gmail.com>
2012-12-06 22:59:24 -0800

    ability to save images locally. more options for api object.
    also: better reporting as to what operation is in progress. added
    .gitignore file.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flickr-API test
+
+## Table of Contents
+
+   * [Running](#running)
+
+## Running
+
+        ruby flickr_http_test.rb
--- flickr_http_test.rb
+++ flickr_http_test.rb
@@ -4,47 +4,32 @@ require 'net/http'
 require 'xmlsimple'
 require 'pp'
 require 'json'
-require_relative 'lib/colorprint'
+require './lib/colorprint'
 
 class Flickr_API
+
+  attr_reader :faves, :favesXML, :titles
 
   def initialize
     @titles = []
     @faves = {}
+    @favesXML = nil
   end
 
   def get_flickr_faves
-    
-    should_open_files_at_end
-    
-    faves_xml = Net::HTTP.get('api.flickr.com', '/services/feeds/photos_faves.gne?id=49782305@N02')
-    @faves = XmlSimple.xml_in(faves_xml)
+    puts 'accessing api.flickr.com ...'
+    @favesXML = Net::HTTP.get('api.flickr.com', '/services/feeds/photos_faves.gne?id=49782305@N02')
+    @faves = XmlSimple.xml_in(@favesXML) # converts XML response to a Ruby hash
+    @faves = @faves['entry']
     
     #pp faves
-    @faves['entry'].each do |entry|
-      puts "#{ColorPrint::green(entry['title'][0])} by #{entry['author'][0]['name'][0]}" rescue ''
-      @titles << entry['title']
+    @faves.each do |photo|
+      puts "#{ColorPrint::green(photo['title'][0])} by #{photo['author'][0]['name'][0]}" rescue ''
+      @titles << photo['title'].first rescue photo['title']
     end
     
     write_titles_to_file
-    open_files_in_browser if @open_photos_at_end
-    
-  end
-
-  private
-  def should_open_files_at_end
-    puts "\ndo you want files opened in the browser at the end? #{ColorPrint::red('y/n')}"
-    @open_photos_at_end = gets.chomp
-    @open_photos_at_end = !!@open_photos_at_end.match(/^(y|yes)/)
-  end
-
-  def write_titles_to_file
-    myStr = @titles.join("\n")
-    titles_file = File.new("titles.txt", "w")
-    titles_file.write(myStr)
-    # titles_file.write("\n\n")
-    # titles_file.write(@faves['entry'].to_json)
-    titles_file.close
+    open_files_in_browser if should_open_files_at_end
   end
 
   def open_files_in_browser
@@ -56,8 +41,50 @@ class Flickr_API
     puts "opening: #{ColorPrint::yellow(second_to_last_file_href)}"
     `open "#{second_to_last_file_href}"`
   end
+
+  def save_favorite(index)
+    image_dir = 'images'
+    create_image_dir image_dir
+    
+    photo = @faves[index]
+    title = photo['title'][0]
+    url = photo['link'][1]['href']
+    uri = URI.parse(url)
+    
+    puts 'getting file...'
+    response = Net::HTTP.get_response(uri)
+    puts 'saving file...'
+    File.open("#{image_dir}/#{title}.jpeg", 'w') do |file|
+      file.write(response.body)
+    end
+  end
+
+  private
   
+  def create_image_dir(dir_name)
+    if File.directory?(dir_name)
+    else
+      puts "creating directory '#{dir_name}'..."
+      Dir.mkdir(dir_name)
+    end
+  end
+  
+  def should_open_files_at_end
+    puts "\ndo you want files opened in the browser at the end? #{ColorPrint::red('y/n')}"
+    @open_photos_at_end = gets.chomp
+    @open_photos_at_end = !!@open_photos_at_end.match(/^(y|yes)/)
+  end
+
+  def write_titles_to_file
+    myStr = @titles.join("\n")
+    File.open("titles.txt", "w") do |file|
+      file.write(myStr)
+    end
+  end
+
 end
   
-f = Flickr_API.new
-f.get_flickr_faves
+# f = Flickr_API.new
+# f.get_flickr_faves
+# sleep(2)
+# f.open_files_in_browser
