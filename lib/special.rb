dc58431562f6cb94d925cdffc9a4846fe67af364
Dominick <dguzzo@gmail.com>
2013-07-30 16:36:01 -0700

    added swirl() and adjusted distort_arc.

--- lib/special.rb
+++ lib/special.rb
@@ -8,7 +8,7 @@ module RMagickBlend
         def self.distort
             image = load_image
             
-            #[UndefinedDistortion=0, AffineDistortion=1, AffineProjectionDistortion=2, ArcDistortion=9, PolarDistortion=10, DePolarDistortion=11, BarrelDistortion=14, BilinearDistortion=6, BilinearForwardDistortion=6, BilinearReverseDistortion=7, PerspectiveDistortion=4, PerspectiveProjectionDistortion=5, PolynomialDistortion=8, ScaleRotateTranslateDistortion=3, ShepardsDistortion=16, BarrelInverseDistortion=15]
+            # UndefinedDistortion AffineDistortion AffineProjectionDistortion ArcDistortion PolarDistortion DePolarDistortion BarrelDistortion BilinearDistortion BilinearForwardDistortion BilinearReverseDistortion PerspectiveDistortion PerspectiveProjectionDistortion PolynomialDistortion ScaleRotateTranslateDistortion ShepardsDistortion BarrelInverseDistortion
             
             distort_operation = Magick::BilinearReverseDistortion
             points = [0, 0, 26, 0, 128, 0, 114, 23, 128, 128, 128, 100, 0, 128, 0, 123]
@@ -25,13 +25,15 @@ module RMagickBlend
             end
         end
         
-        def self.distort_arc(variants)
+        def self.distort_arc(variants, options = {})
+            options = {arc_angle: 60, rotate_angle: 0, top_radius: 100, bottom_radius: 100}.merge(options)
             image = load_image
             
             variants.times do |i|
+                arc_angle = options[:arc_angle] || 60
                 #args: arc_angle   rotate_angle   top_radius   bottom_radius
-                mod_image = image.distort(Magick::ArcDistortion, 60 + 20*i )
-                save_image(mod_image, i)
+                mod_image = image.dup.distort(Magick::ArcDistortion, [arc_angle + 20*i, options[:rotate_angle], options[:top_radius], options[:bottom_radius] ])
+                save_image(mod_image, "assets/images/distort_test/sample_distort_#{i}")
             end
         end
 
@@ -40,15 +42,28 @@ module RMagickBlend
             anim = Magick::ImageList.new(*image_names)
             anim.write("#{dir}/animated.gif")
         end
+   
+        def self.swirl(deg)
+            image = load_image
+            mod_image = image.swirl(clamp_degrees(deg))
+            save_image(mod_image, "assets/images/swirl_test.jpg")
+        end
         
         private
+
+        def self.clamp_degrees(deg)
+            max = 360*24
+            min = max * -1
+            return max if deg > max #heuristic
+            return min if deg < min
+            deg
+        end
         
         def self.load_image
             Magick::Image.read('assets/images/batch-8-source/9252445443_5c5c679774_c.jpg').first
         end
         
-        def self.save_image(image, index)
-            path = "assets/images/distort_test/sample_distort_#{index}.jpg"
+        def self.save_image(image, path)
             puts "writing file: #{path}"
             image.write(path)
         end
