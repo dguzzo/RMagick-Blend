fa86c7023a3b211666584df2687a12474c255055
Dominick Guzzo <dguzzo@gmail.com>
2015-02-11 16:15:48 -0800

    remove old method
    …it’s since been moved to ImageUtils

--- lib/rmagick-blend/compositing.rb
+++ lib/rmagick-blend/compositing.rb
@@ -72,17 +72,6 @@ module RMagickBlend
     end
     # end composite_images
 
-    
-    def self.match_image_sizes(src, dest)
-      # based on width
-      if src.bounding_box.x >= dest.bounding_box.x 
-        src = src.resize(dest.bounding_box.x, dest.bounding_box.y)
-      else
-        dest = dest.resize(src.bounding_box.x, src.bounding_box.y)
-      end
-      [src, dest]
-    end
-
   end
 end
 
