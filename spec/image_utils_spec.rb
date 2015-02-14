c5e515dcafe6fdb8916bbd78427f620a4409387b
Dominick Guzzo <dguzzo@gmail.com>
2015-02-13 20:14:23 -0800

    fix false negative tests...
    Images created via the constructor like this need to have a fill
    otherwise their bounding-box is not set up properly and report their
    dimensions wrong.

--- spec/image_utils_spec.rb
+++ spec/image_utils_spec.rb
@@ -6,21 +6,22 @@ describe "ImageUtils" do
   describe "match_image_sizes" do
 
     src, dest = nil
+    fill = Magick::HatchFill.new('red')
 
     it "resizes dest image if source image is smaller" do
-      src, dest = Magick::Image.new(100, 100), Magick::Image.new(200, 200) 
+      src, dest = Magick::Image.new(100, 100, fill), Magick::Image.new(200, 200, fill)
       src, dest = RMagickBlend::ImageUtils::match_image_sizes(src, dest)
       expect(dest.bounding_box.width).to eq(src.bounding_box.width)
     end
     
     it "resizes src image if dest image is smaller" do
-      src, dest = Magick::Image.new(100, 100), Magick::Image.new(50, 200) 
+      src, dest = Magick::Image.new(100, 100, fill), Magick::Image.new(50, 200, fill) 
       src, dest = RMagickBlend::ImageUtils::match_image_sizes(src, dest)
       expect(src.bounding_box.width).to eq(dest.bounding_box.width)
     end
 
     it "does not resize if both images are the same size" do
-      src, dest = Magick::Image.new(200,200), Magick::Image.new(200, 200) 
+      src, dest = Magick::Image.new(200, 200, fill), Magick::Image.new(200, 200, fill) 
       src, dest = RMagickBlend::ImageUtils::match_image_sizes(src, dest)
       expect(dest.bounding_box.width).to eq(src.bounding_box.width)
     end
